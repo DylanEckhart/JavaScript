5077c808297f752d424f58078256f3d86c479373
Dylan-Eckhart <deckhart@roc-dev.com>
2020-10-08 09:35:28 +0200

    basis.js is added and changed

--- Opdrachten/Basis/basis.js
+++ Opdrachten/Basis/basis.js
@@ -1,1 +1,14 @@
 console.log("Hello World")
+
+for(var i = 0; i < 10; i++) {
+    console.log("De waarde van i is " + i);
+}
+
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalwielen: 4,
+    kleur: "Blauw"
+}
+
+console.log(mijnauto.kleur)
