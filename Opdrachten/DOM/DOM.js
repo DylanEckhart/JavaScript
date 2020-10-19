948a4654462d093e92626ee38a4859866d928280
Dylan-Eckhart <deckhart@roc-dev.com>
2020-10-19 10:32:17 +0200

    Changes in DOM.js

--- Opdrachten/DOM/DOM.js
+++ Opdrachten/DOM/DOM.js
@@ -9,3 +9,7 @@ document.getElementById("bl1").style.backgroundColor = "blue";
 document.getElementById("bl2").style.backgroundColor = "green";
 document.getElementById("bl3").style.backgroundColor = "yellow";
 document.getElementById("bl4").style.backgroundColor = "red";
+
+document.getElementById("bl2").style.marginTop = "25px";
+document.getElementById("bl3").style.marginTop = "50px";
+
