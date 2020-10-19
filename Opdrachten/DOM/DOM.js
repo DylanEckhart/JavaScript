ecde1d12c1fa85e27977e65d85a49d1615409620
Dylan-Eckhart <deckhart@roc-dev.com>
2020-10-19 10:23:40 +0200

    index.html, DOM.js and style.css has been created

---
+++ Opdrachten/DOM/DOM.js
@@ -0,0 +1,11 @@
+//Opdracht 2.1
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel"
+document.getElementById("subkop").innerText = "Dit is de subkop"
+document.getElementById("aap1").src = "IMG/aap1.jpg"
+document.getElementById("aap2").src = "IMG/aap2.jpeg"
+
+//Opdracht 2.2
+document.getElementById("bl1").style.backgroundColor = "blue";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
