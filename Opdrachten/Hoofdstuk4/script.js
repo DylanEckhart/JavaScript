3839096b690ec583d1248ab0d769ca4a634a5910
Dylan-Eckhart <deckhart@roc-dev.com>
2020-10-21 11:27:32 +0200

    Changed .png to .jpg

--- Opdrachten/Hoofdstuk4/script.js
+++ Opdrachten/Hoofdstuk4/script.js
@@ -47,20 +47,20 @@ var crooks = [1,2,3,4,5];
 var count = 0;
 
 var crook1 = document.getElementById("Crook1");
-crook1.style.backgroundImage = "url('IMGCrook/crookOgen1.png')";
+crook1.style.backgroundImage = "url('IMGCrook/crookOgen1.jpg')";
 var crook2 = document.getElementById("Crook2");
-crook2.style.backgroundImage = "url('IMGCrook/crookNeus1.png')";
+crook2.style.backgroundImage = "url('IMGCrook/crookNeus1.jpg')";
 var crook3 = document.getElementById("Crook3");
-crook3.style.backgroundImage = "url('IMGCrook/crookMond1.png')";
+crook3.style.backgroundImage = "url('IMGCrook/crookMond1.jpg')";
 
 crook1.addEventListener("click", function() {
-    crook1.style.backgroundImage = "url('IMGCrook/crookOgen" + getCrook() + ".png')";
+    crook1.style.backgroundImage = "url('IMGCrook/crookOgen" + getCrook() + ".jpg')";
 });
 crook2.addEventListener("click", function() {
-    crook2.style.backgroundImage = "url('IMGCrook/crookNeus" + getCrook() + ".png')";
+    crook2.style.backgroundImage = "url('IMGCrook/crookNeus" + getCrook() + ".jpg')";
 });
 crook3.addEventListener("click", function() {
-    crook3.style.backgroundImage = "url('IMGCrook/crookMond" + getCrook() + ".png')";
+    crook3.style.backgroundImage = "url('IMGCrook/crookMond" + getCrook() + ".jpg')";
 });
 
 function getCrook() {
