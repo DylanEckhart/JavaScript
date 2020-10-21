840c15c0cd3195a8a34fee747657ec6138412f5e
Dylan-Eckhart <deckhart@roc-dev.com>
2020-10-21 09:51:27 +0200

    Fixed the getDriver function in script.js

--- Opdrachten/Hoofdstuk4/script.js
+++ Opdrachten/Hoofdstuk4/script.js
@@ -24,8 +24,26 @@ mijnauto.toeteren();
 //Opdracht 4.2
 
 var plaatjes = [1,2,3,4,5,6,7,8,9];
+var teller = 0;
 var slideholder = document.getElementById("slideholder");
-slideholder.style.backgroundImage = "url('/IMG/f1.jpg')";
+slideholder.style.backgroundImage = "url('IMG/f1.jpg')";
+
+slideholder.addEventListener("click", function() {
+    slideholder.style.backgroundImage = "url('IMG/f" + getDriver() + ".jpg')";
+});
+
+function getDriver() {
+    if(teller >= plaatjes.length) {
+        teller = 1;
+    } else {
+        teller++;
+    }
+    return teller;
+}
+
+//Opdracht 4.3
+
+
 
 
 
