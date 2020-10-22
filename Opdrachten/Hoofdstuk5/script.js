ad4b84fea2950ea156d852a8c85a2ed8ca40410b
Dylan-Eckhart <deckhart@roc-dev.com>
2020-10-22 11:35:15 +0200

    createDriverPictures function

--- Opdrachten/Hoofdstuk5/script.js
+++ Opdrachten/Hoofdstuk5/script.js
@@ -19,10 +19,24 @@ function createDriverPictures() {
     for (var i = 0; i < pictureHolders.length; i++) {
         favoriet = document.createElement("div");
         favoriet.className = "favoriet";
+        favoriet.id = "favoriet_" + (i + 1);
         driverFoto = document.createElement("IMG");
         driverFoto.src = "IMG/f" + (i + 1) + ".jpg";
+        driverFoto.id = (i + 1);
+        driverFoto.addEventListener("click", function () {
+            maakFavoriet(this.id);
+        });
         pictureHolders[i].appendChild(favoriet);
         pictureHolders[i].appendChild(driverFoto);
     }
 }
+
+// Favoriet maken van foto
+function maakFavoriet(id) {
+    favoriet = document.getElementById("favoriet_" + id);
+    favoriet.style.backgroundImage = "url('IMG/heart.jpg')";
+
+
+}
+
 
