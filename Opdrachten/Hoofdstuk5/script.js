8b4836896af8bd275a0cd1d9eab5716c0c007d62
Dylan-Eckhart <deckhart@roc-dev.com>
2020-10-22 11:03:23 +0200

    createDriverPictures function

--- Opdrachten/Hoofdstuk5/script.js
+++ Opdrachten/Hoofdstuk5/script.js
@@ -1,12 +1,25 @@
-// Create pictureHolders
+// Invocations
 pictures = document.getElementById("pictures");
 createPictureHolders();
+createDriverPictures();
+
+// Create pictureHolders
 function createPictureHolders() {
     for (var i = 0; i < 9; i++) {
         pictureHolder = document.createElement("div");
         pictureHolder.className = "driverPicture";
-        pictureHolder.id = "picture-holder" + i;
+        pictureHolder.id = "picture-holder-" + i;
         pictures.appendChild(pictureHolder);
+    }
+}
+
+// Plaatjes in pictureHolders zetten
+function createDriverPictures() {
+    pictureHolders = document.getElementsByClassName("driverPicture");
+    for (var i = 0; 9 < pictureHolders.length; i++) {
+        driverFoto = document.createElement("img");
+        driverFoto.src = "url('IMG/f" + (i + 1) + ".jpg')";
+        pictureHolders[i].appendChild(driverFoto);
     }
 }
 
