606eebc0cda5d1edd88f6f964c589f49c5625695
Dylan-Eckhart <deckhart@roc-dev.com>
2020-10-27 12:53:54 +0100

    Made functions

--- Opdrachten/Memory Game/script.js
+++ Opdrachten/Memory Game/script.js
@@ -1,2 +1,11 @@
-var beurt = "F1 Memory"
+// Variabelen
+var beurt = " Heeft de beurt"
+var speler1 = "Dylan"
+var speler2 = "Timo"
+var puntenSpeler1 = 0;
+var puntenSpeler2 = 0;
+
+// Text in top-bar
 document.getElementById("tekstbeurt").innerHTML = beurt;
+document.getElementById("textSpeler1").innerHTML = speler1 + " : " + puntenSpeler1;
+document.getElementById("textSpeler2").innerHTML = speler2 + " : " + puntenSpeler2;
