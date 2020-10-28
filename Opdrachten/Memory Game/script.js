7dcef2bec5da70acc0dfd30723e2ab30cdafef5a
Dylan-Eckhart <deckhart@roc-dev.com>
2020-10-28 15:53:19 +0100

    Removed almost all functions due to miscomunication in classes

--- Opdrachten/Memory Game/script.js
+++ Opdrachten/Memory Game/script.js
@@ -5,10 +5,13 @@ var beurt = " heeft de beurt"
 var puntenSpeler1 = 0;
 var puntenSpeler2 = 0;
 var pictures;
+var aantalFoto = [1,2,3,4,5,6,7,8,9];
 var aantal = 0;
 var i = 1;
+
 Boolean; beurtSpeler1 = true;
 Boolean; zet = true;
+
 
 // DOM argumenten
 document.getElementById("tekstbeurt").innerHTML = speler1 + " mag beginnen";
