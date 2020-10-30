7db60bd159ae9d2cfb64ca68027f5c41a8a8d94c
Dylan-Eckhart <deckhart@roc-dev.com>
2020-10-30 17:11:52 +0100

    Changed style.css and made class "container"

--- Opdrachten/Memory Game/script.js
+++ Opdrachten/Memory Game/script.js
@@ -5,35 +5,19 @@ var beurt = " heeft de beurt"
 
 var puntenSpeler1 = 0;
 var puntenSpeler2 = 0;
-var pictures;
-var aantal = 0;
 
 Boolean; beurtSpeler1 = true;
 Boolean; zet = true;
-
-// Aanroepen Functies
-AddID()
 
 // DOM argumenten
 document.getElementById("tekstbeurt").innerHTML = speler1 + " mag beginnen";
 document.getElementById("textSpeler1").innerHTML = speler1 + " : " + puntenSpeler1;
 document.getElementById("textSpeler2").innerHTML = speler2 + " : " + puntenSpeler2;
 
-// Overlay
-overlay = document.getElementsByClassName("overlay");
-
 // Laden van foto's
 let pictureholders = document.getElementsByClassName("picture-holder");
 for (let i = 0; i < pictureholders.length; i++) {
-    pictureholders[i].style.background = 'url("IMG/2.png")';
-}
-
-function AddID() {
-    var cls = document.getElementsByClassName("foto");
-    for (var i = 0; i < 18; i++) {
-        cls[i].id = "img_" + (i + 1);
-        console.log(cls[i])
-    }
+    pictureholders[i].style.background = 'url("IMG/f3.png")';
 }
 
 // Puntensysteem, zetsysteem & beurtsysteem
