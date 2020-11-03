78b1187a3a27bddddc966a97af473af8a0e495f1
Dylan-Eckhart <deckhart@roc-dev.com>
2020-11-03 18:56:15 +0100

    Changed style.css and made class "container"

--- Opdrachten/Memory Game/script.js
+++ Opdrachten/Memory Game/script.js
@@ -1,43 +1,4 @@
-// Variabelen
-let speler1 = "Dylan"
-let speler2 = "Timo"
-let beurt = " heeft de beurt"
 
-let puntenSpeler1 = 0;
-let puntenSpeler2 = 0;
 
-Boolean; beurtSpeler1 = true;
-Boolean; zet = true;
-
-// DOM argumenten
-document.getElementById("tekstbeurt").innerHTML = speler1 + " mag beginnen";
-document.getElementById("textSpeler1").innerHTML = speler1 + " : " + puntenSpeler1;
-document.getElementById("textSpeler2").innerHTML = speler2 + " : " + puntenSpeler2;
-
-// Laden van foto's
-let pictureholders = document.getElementsByClassName("picture-holder");
-for (let i = 0; i < pictureholders.length; i++) {
-    pictureholders[i].style.background = 'url("IMG/f3.png")';
-}
-
-// Puntensysteem, zetsysteem & beurtsysteem
-function beurtsysteem() {
-    if (puntenSpeler1 % 5 === 0) {
-        beurtSpeler1 = false;
-        zet = false;
-    } else if (puntenSpeler2 % 5 === 0) {
-        beurtSpeler1 = true;
-        zet = true;
-    }
-}
-
-function zetsysteem () {
-    beurtsysteem()
-    if (zet === true) {
-        document.getElementById("tekstbeurt").innerHTML = speler1 + beurt;
-    } else if (zet === false) {
-        document.getElementById("tekstbeurt").innerHTML = speler2 + beurt;
-    }
-}
 
 
