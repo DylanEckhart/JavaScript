29ce905af1541fd5f39dc0e75b404cf2f13b8255
Dylan-Eckhart <deckhart@roc-dev.com>
2020-11-03 19:44:50 +0100

    Changed style.css and made class "container"

--- Opdrachten/Memory Game/script.js
+++ Opdrachten/Memory Game/script.js
@@ -1,4 +1,19 @@
+// Variabelen
+var winner = null;
+var zetten = 0;
+var laatstGekilkt = null;
+var volgendeZet = true;
 
+var puntenDylan = 0;
+var puntenTimo = 0;
+var spelers = ["Dylan", "Timo"];
+
+var openKaarten = [null, null];
+var spelerAanZet = Math.floor(Math.random() * 2);
+
+// Aanroepen Methodes
+toonPlayer();
+toonScore();
 
 
 
