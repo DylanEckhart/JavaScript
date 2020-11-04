06acecfa43a3f84e1e2adbcdf1e29d1e23b53fa8
Dylan-Eckhart <deckhart@roc-dev.com>
2020-11-04 13:03:39 +0100

    Changed style.css and made class "container"

--- Opdrachten/Memory Game/script.js
+++ Opdrachten/Memory Game/script.js
@@ -29,5 +29,19 @@ function opdrachtGeklikteKaart() {
     }
 }
 
+// Methode voor overlay weghalen
+function reageerOpKlik() {
+    var alleOverlays = document.getElementsByClassName("overlay");
+    for (let a = 0; a < allBlocks.length ; a++) {
+        if (driverArray[a] !== null) {
+            alleOverlays[a].style.background = "white";
+        }
+    }
+    button.style.display = 'none';
+    zetten = 0;
+    laatstGekilkt = null;
+    volgendeZet = true;
+}
+
 
 
